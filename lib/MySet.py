e75ea7523d7b8824e1a568f65f329fb7ac16abd5
mtgrt <mtgrt1369@gmail.com>
2024-06-11 11:37:53 +0300

    tried

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,18 @@
-class MySet:
 
-    pass
+class MySet:
+    def __init__(self, iterable=None):
+        self.elements = set()
+        if iterable:
+            self.elements.update(iterable)
+
+    def add(self, element):
+        self.elements.add(element)
+
+    def delete(self, element):
+        self.elements.discard(element)
+
+    def has(self, element):
+        return element in self.elements
+
+    def size(self):
+        return len(self.elements)
